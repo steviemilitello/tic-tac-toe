3d5e278df029874046317600a3f5492891820f69
STEVIEMILITELLO <steviemilitello@gmail.com>
2022-02-14 04:34:52 -0500

    commit 3 - added several working elements however could not get more than 1 div to be clickable and several other issues

--- js/app.js
+++ js/app.js
@@ -5,46 +5,70 @@ const message = document.querySelector("#message")
 
 // A variable to keep track of moves, indicate whether or not to draw X or O 
 
-// const player = {
-//     currentChoice: null
-// }
+const player = {
+    currentChoice: null
+}
+const computer = {
+    currentChoice: null
+}
+const xChoice = document.createTextNode("X")
 
-// const xChoice = "X"
-// const oChoice = "O"
+const oChoice = document.createTextNode("O")
 
-// const playerChoice = () => {
-//     if (player.currentChoice === xChoice) {
-//         // ??? add O ??? ///
-//     } else (player.currentChoice === oChoice)
-//        // ??? add X ????
-// }
+const playerChoice = () => {
+    if (player.currentChoice === xChoice) {
+        computer.currentChoice === oChoice
+    } else (player.currentChoice === oChoice)
+    return playerChoice
+}
 
 // A user should be able to click on different squares to make a move.
 
-const addSymbol = (event) => {
-    // add X or O symbol
-    const symbol = document.createTextNode("X")
-    console.log("the symbol is", symbol)
+const addMove = (event) => {
+    // add player choice 
+    const playerMove = document.createTextNode("X")
+    // access player choice 
+    // check to see if x or o is current choice
+    // console log to make sure it worked
+    console.log("the symbol is", playerMove)
     // select game square
-    gameSquare.classList.add("game-square")
     // add symbol to the selected square
-    gameSquare.appendChild(symbol)
+    gameSquare.appendChild(playerMove)
     // sends a message to user that symbol has been added 
     message.innerText = `added symbol to board`
 
 }
 
+// const makeBoard = () => {
+// 	// if there are already divs in gameSquare, clear them out
+// 	while (gameSquare.firstChild) {
+// 		// if there is a child of gameSquare, remove it
+// 		gameSquare.removeChild(gameSquare.firstChild)
+// 	}
+// 	// square creation loop
+// 	for (let i = 1; i < 10; i++) {
+// 		// console log made sure the loop runs correctly, at the right time
+// 		console.log('make a div')
+// 		// generate a div every loop
+// 		const div = document.createElement('div')
+//         div.classList.add("div")
+// 		// append those divs 
+// 		gameSquare.appendChild(div)
+//         div.addEventListener("click", addMove)
+//     }
+//     message.innerText = 'squares successfully generated'
+// }
+
 // Every click will alternate between marking an X and O
 
 // Upon marking of an individual cell, use JavaScript to add an X or O to the cell, according to whose turn it is.
-
-// A cell should not be able to be replayed once marked.
 
 // You should not be able to click remaining empty cells after the game is over.
 
 // Add a reset button that will clear the contents of the board.
 
 const clearBoard = () => {
+    location.reload()
     while (gameSquare.firstChild) {
     // if there is a child of the gameBoard it is removed
         gameSquare.removeChild(gameSquare.firstChild)
@@ -63,6 +87,8 @@ message.innerText = "cleared board"
 // DOM content
 
 document.addEventListener("DOMContentLoaded", () => {
-    // gameSquare.addEventListener("click", addSymbol)
+    // A cell should not be able to be replayed once marked
+    gameSquare.addEventListener("click", addMove, {once: true})
     resetButton.addEventListener("click", clearBoard)
+    makeBoard()
 })
