6370cb33f0148c4b79f41f98f2ff0da9652c7815
STEVIEMILITELLO <steviemilitello@gmail.com>
2022-02-18 06:08:43 -0500

    made all divs clickable, however still stuck on how to alternate btw X and O also well as win conditions

--- js/app.js
+++ js/app.js
@@ -1,69 +1,38 @@
 const gameBoard = document.querySelector("#container")
 const resetButton = document.querySelector("#reset")
 const gameSquare = document.querySelector(".game-square")
+const square1 = document.querySelector("#square-1")
+const square2 = document.querySelector("#square-2")
+const square3 = document.querySelector("#square-3")
+const square4 = document.querySelector("#square-4")
+const square5 = document.querySelector("#square-5")
+const square6 = document.querySelector("#square-6")
+const square7 = document.querySelector("#square-7")
+const square8 = document.querySelector("#square-8")
+const square9 = document.querySelector("#square-9")
 const message = document.querySelector("#message")
 
 // A variable to keep track of moves, indicate whether or not to draw X or O 
 
-const player = {
-    currentChoice: null
-}
-const computer = {
-    currentChoice: null
-}
-const xChoice = document.createTextNode("X")
+const playerMoveX = "X"
+const playerMoveO = "O"
+let player = playerMoveX
 
-const oChoice = document.createTextNode("O")
-
-const playerChoice = () => {
-    if (player.currentChoice === xChoice) {
-        computer.currentChoice === oChoice
-    } else (player.currentChoice === oChoice)
-    return playerChoice
-}
-
-// A user should be able to click on different squares to make a move.
+// A user should be able to click on different squares to make a moves
+// Every click will alternate between marking an X and O
+// Upon marking of an individual cell, use JavaScript to add an X or O to the cell, according to whose turn it is.
+    // wasn't able to do this, only able to add X or O to square
 
 const addMove = (event) => {
-    // add player choice 
-    const playerMove = document.createTextNode("X")
-    // access player choice 
-    // check to see if x or o is current choice
-    // console log to make sure it worked
-    console.log("the symbol is", playerMove)
-    // select game square
-    // add symbol to the selected square
-    gameSquare.appendChild(playerMove)
-    // sends a message to user that symbol has been added 
-    message.innerText = `added symbol to board`
-
+    event.target.append(player)
+    if (player === playerMoveX) {
+        player === playerMoveO 
+    } else {
+        player === playerMoveX
+    }
+    // Display a message to indicate which turn is about to be played.
+    message.innerText = `it is ${player}'s turn!`
 }
-
-// const makeBoard = () => {
-// 	// if there are already divs in gameSquare, clear them out
-// 	while (gameSquare.firstChild) {
-// 		// if there is a child of gameSquare, remove it
-// 		gameSquare.removeChild(gameSquare.firstChild)
-// 	}
-// 	// square creation loop
-// 	for (let i = 1; i < 10; i++) {
-// 		// console log made sure the loop runs correctly, at the right time
-// 		console.log('make a div')
-// 		// generate a div every loop
-// 		const div = document.createElement('div')
-//         div.classList.add("div")
-// 		// append those divs 
-// 		gameSquare.appendChild(div)
-//         div.addEventListener("click", addMove)
-//     }
-//     message.innerText = 'squares successfully generated'
-// }
-
-// Every click will alternate between marking an X and O
-
-// Upon marking of an individual cell, use JavaScript to add an X or O to the cell, according to whose turn it is.
-
-// You should not be able to click remaining empty cells after the game is over.
 
 // Add a reset button that will clear the contents of the board.
 
@@ -73,10 +42,7 @@ const clearBoard = () => {
     // if there is a child of the gameSquare it is removed
         gameSquare.removeChild(gameSquare.firstChild)
     }
-message.innerText = "cleared board"
 }
-
-// Display a message to indicate which turn is about to be played.
 
 // Detect draw conditions (ties/cat's game)
 
@@ -88,7 +54,16 @@ message.innerText = "cleared board"
 
 document.addEventListener("DOMContentLoaded", () => {
     // A cell should not be able to be replayed once marked
-    gameSquare.addEventListener("click", addMove, {once: true})
+    // You should not be able to click remaining empty cells after the game is over
+    square1.addEventListener("click", addMove, {once: true})
+    square2.addEventListener("click", addMove, {once: true})
+    square3.addEventListener("click", addMove, {once: true})
+    square4.addEventListener("click", addMove, {once: true})
+    square5.addEventListener("click", addMove, {once: true})
+    square6.addEventListener("click", addMove, {once: true})
+    square7.addEventListener("click", addMove, {once: true})
+    square8.addEventListener("click", addMove, {once: true})
+    square9.addEventListener("click", addMove, {once: true})
     resetButton.addEventListener("click", clearBoard)
-    makeBoard()
 })
+
